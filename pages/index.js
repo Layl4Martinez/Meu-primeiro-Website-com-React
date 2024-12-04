4fb0f492773bf954d224f77beee6743f0b60e51c
LaylaBoba <laylamartinez@duck.com>
2024-12-04 16:02:50 -0300

    meu primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>Meu primeiro website! :3</h1>
+            <p>Paragrafo a baixo do título wooww</p>
+            <p>yupii</p>
+        </div>
+    )
+}
+
+export default Home
